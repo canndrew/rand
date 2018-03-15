e8280e3425d4709b7cdecf43ad13d94f81bd34dd
Travis CI User <travis@example.org>
2018-03-15 16:27:47 +0000

    Documentation for rust-lang-nursery/rand@fd0bd08

---
+++ implementors/rand/trait.CryptoRng.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rand"] = [{text:"impl <a class=\"trait\" href=\"rand/trait.CryptoRng.html\" title=\"trait rand::CryptoRng\">CryptoRng</a> for <a class=\"struct\" href=\"rand/jitter/struct.JitterRng.html\" title=\"struct rand::jitter::JitterRng\">JitterRng</a>",synthetic:false,types:["rand::jitter::JitterRng"]},{text:"impl <a class=\"trait\" href=\"rand/trait.CryptoRng.html\" title=\"trait rand::CryptoRng\">CryptoRng</a> for <a class=\"struct\" href=\"rand/struct.ChaChaRng.html\" title=\"struct rand::ChaChaRng\">ChaChaRng</a>",synthetic:false,types:["rand::prng::chacha::ChaChaRng"]},{text:"impl <a class=\"trait\" href=\"rand/trait.CryptoRng.html\" title=\"trait rand::CryptoRng\">CryptoRng</a> for <a class=\"struct\" href=\"rand/struct.Hc128Rng.html\" title=\"struct rand::Hc128Rng\">Hc128Rng</a>",synthetic:false,types:["rand::prng::hc128::Hc128Rng"]},{text:"impl <a class=\"trait\" href=\"rand/trait.CryptoRng.html\" title=\"trait rand::CryptoRng\">CryptoRng</a> for <a class=\"struct\" href=\"rand/struct.ThreadRng.html\" title=\"struct rand::ThreadRng\">ThreadRng</a>",synthetic:false,types:["rand::thread_rng::ThreadRng"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
